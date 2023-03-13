f95e512397eb4c643be156bfe2aaba83841411b8
DearJao <jvvr2013@gmail.com>
2023-03-13 16:55:38 -0300

    função que recupera url da proxima pagina / req-3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,7 +31,10 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    page_list = selector.css("a.next::attr(href)").get()
+
+    return page_list
 
 
 # Requisito 4
